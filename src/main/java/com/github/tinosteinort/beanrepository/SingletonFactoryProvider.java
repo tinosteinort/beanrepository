b95641c0e85279dbc64ecad720d2ecc2be57ddc3
Tino Steinort <tinosteinort@live.de>
2016-12-01 15:27:11 +0100

    threadsafe SingletonFactoryProvider

--- src/main/java/com/github/tinosteinort/beanrepository/SingletonFactoryProvider.java
+++ src/main/java/com/github/tinosteinort/beanrepository/SingletonFactoryProvider.java
@@ -1,26 +1,44 @@
 package com.github.tinosteinort.beanrepository;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
 
 public class SingletonFactoryProvider implements BeanProvider {
 
     private final String repositoryId;
     private final Function<BeanAccessor, Factory> creator;
-//    private final SingletonProvider singletonProvider;
+    private volatile Object instance;
+    private final Lock lock = new ReentrantLock();
 
     SingletonFactoryProvider(final String repositoryId, final Function<BeanAccessor, Factory> creator) {
         this.repositoryId = repositoryId;
         this.creator = creator;
-//        final SingletonProvider singletonProvider
-//        this.singletonProvider = new SingletonProvider(repositoryId, (repo) -> this.getBean(repo, false));
     }
 
     @Override public <T> T getBean(final BeanRepository repository, final boolean dryRun) {
-        final Factory factory = creator.apply(repository.accessor());
-        if (dryRun) {
-            return null;
+        if (instance == null) {
+            return createAndGetBean(repository, dryRun);
         }
-        return (T) factory.createInstance();
+        return (T) instance;
+    }
+
+    private <T> T createAndGetBean(final BeanRepository repository, final boolean dryRun) {
+        lock.lock();
+        try {
+            if (instance == null) {
+                final Factory factory = creator.apply(repository.accessor());
+                if (!dryRun) {
+                    final Object beanInstance = factory.createInstance();
+                    repository.postConstruct(beanInstance);
+                    instance = beanInstance;
+                }
+            }
+            return (T) instance;
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     @Override public String getRepositoryId() {
